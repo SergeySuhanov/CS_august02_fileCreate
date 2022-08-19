f34e28b64828c3ace114e0860d9d868317daa337
СергейС <beent@yandex.ru>
2022-08-19 19:15:14 +0300

    smtg changed

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CS_august02
@@ -10,10 +11,21 @@ namespace CS_august02
     {
         static void Main(string[] args)
         {
-            FileReader reader = new FileReader();
-            reader.OpenDirectioryInC();
+            //FileReader reader = new FileReader();
+            //reader.OpenDirectioryInC();
             //reader.CreateFileInC();
-            reader.OpenFileInC();
+            //reader.OpenFileInC();
+
+            /*string a = "Mob is stinky";
+            string pattern = "Mob";
+            var find = Regex.Match(a, pattern);
+            Console.WriteLine(find.Success);
+            a = Regex.Replace(a, pattern, "Fab");*/
+
+            string input = Console.ReadLine();
+            input = Regex.Replace(input, "a", "3");
+            input = Regex.Replace(input, "\\.", ":");
+            Console.WriteLine(input);
 
             Console.ReadLine();
         }
